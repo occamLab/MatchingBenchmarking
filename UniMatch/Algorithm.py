ed5a0d3efa6cf365e0b76eb8986d964542b0b836
Marc Eftimie <marc.eftimie@gmail.com>
2022-06-17 14:24:00 -0400

    Fixing class names

--- UniMatch/Algorithm.py
+++ UniMatch/Algorithm.py
@@ -20,7 +20,7 @@ class SuperGlue(AlgorithmObj):
     def __init__(self):
         pass
 
-class Orb1(AlgorithmObj):
+class Orb(AlgorithmObj):
 
     def __init__(self, img1, img2):
         super().__init__(img1, img2)
