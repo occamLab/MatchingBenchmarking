3d58597aa8d142748f8e8bb75a378eed9fea3e37
Marc Eftimie <marc.eftimie@gmail.com>
2022-06-17 15:14:57 -0400

    Refactored algorithm classes

--- UniMatch/Algorithm.py
+++ UniMatch/Algorithm.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 from UniMatch import UNIMatch
 
 class AlgorithmObj(ABC):
-    def __init__(self, img1, img2):
-        self.img1 = img1
-        self.img2 = img2
+    def __init__(self, query_image, train_image):
+        self.query_image = query_image
+        self.train_image = train_image
     
     @abstractmethod
     def draw_matches(self):
@@ -22,72 +22,79 @@ class SuperGlue(AlgorithmObj):
 
 class Orb(AlgorithmObj):
 
-    def __init__(self, img1, img2):
-        super().__init__(img1, img2)
-        self.matches, self.kp1, self.kp2 = self.get_matches()
+    def __init__(self, query_image, train_image):
+        super().__init__(query_image, train_image)
+        self.matches, self.query_keypoints, self.train_keypoints = self.get_matches()
     
     def get_matches(self):
+        # Initiate ORB algorithm
         orb = cv2.ORB_create()
-        kp1, des1 = orb.detectAndCompute(self.img1, None)
-        kp2, des2 = orb.detectAndCompute(self.img2, None)
+
+        # Find the keypoints and descriptors with ORB
+        query_keypoints, des1 = orb.detectAndCompute(self.query_image, None)
+        train_keypoints, des2 = orb.detectAndCompute(self.train_image, None)
+
+        # Get matches from keypoints and descriptors
         bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
         matches = bf.match(des1, des2)
-        return sorted(matches, key=lambda x: x.distance), kp1, kp2
+
+        # TODO: Add ratio test for ORB algorithm
+        return sorted(matches, key=lambda x: x.distance), query_keypoints, train_keypoints
 
     def draw_matches(self):
-        # draw first 10 matches
-        img3 = cv2.drawMatches(self.img1, self.kp1, self.img2, self.kp2, self.matches[:50], self.img2, flags=2)
+        # Draw first 10 matches
+        img3 = cv2.drawMatches(self.query_image, self.query_keypoints, self.train_image, self.train_keypoints, self.matches[:50], self.train_image, flags=2)
         plt.imshow(img3), plt.show()
     
     def get_uni_matches(self):
+        # Creates 
         coord_array = []
         for match in self.matches:
-            x1, y1 = self.kp1[match.queryIdx].pt
-            x2, y2 = self.kp2[match.trainIdx].pt
-            for x in [x1, y1, x2, y2]:
-                coord_array.append(x)
-        # create list of UNIMatch objects
-        UNIlist = []
-        for i in range(0, len(coord_array), 4):
-            UNIlist.append(UNIMatch(coord_array[i], coord_array[i+1], coord_array[i+2], coord_array[i+3]))
+            x1, y1 = self.query_keypoints[match.queryIdx].pt
+            x2, y2 = self.train_keypoints[match.trainIdx].pt
+            coord_array.extend([x1, y1, x2, y2])
+
+        # Create list of UNIMatch objects
+        uniMatches = [UNIMatch(coord_array[i], coord_array[i + 1], coord_array[i + 2], coord_array[i + 3]) for i in range(0, len(coord_array), 4)]
         
-        return UNIlist
+        return uniMatches
 
 class Sift(AlgorithmObj):
-    def __init__(self, img1, img2):
-        super().__init__(img1, img2)
-        self.good_matches, self.kp1, self.kp2 = self.get_matches()
+
+    def __init__(self, query_image, train_image):
+        super().__init__(query_image, train_image)
+        self.filtered_matches, self.query_keypoints, self.train_keypoints = self.get_matches()
 
     def get_matches(self):
-        # Initiate SIFT detector
+        # Initiate SIFT algorithm
         sift = cv2.SIFT_create()
-        # find the keypoints and descriptors with SIFT
-        kp1, des1 = sift.detectAndCompute(self.img1, None)
-        kp2, des2 = sift.detectAndCompute(self.img2, None)
+
+        # Find the keypoints and descriptors with SIFT
+        query_keypoints, query_descriptors = sift.detectAndCompute(self.query_image, None)
+        train_keypoints, train_descriptors = sift.detectAndCompute(self.train_image, None)
+
+        # Get matches from keypoints and descriptors
         bf = cv2.BFMatcher(cv2.NORM_L2)
-        matches = bf.knnMatch(des1, des2, k=2)
+        matches = bf.knnMatch(query_descriptors, train_descriptors, k=2)
+
         # Apply ratio test (currently set to 1 so it does nothing)
-        good_matches = [m for m, n in matches if m.distance < 1 * n.distance]
-        # for m, n in matches:
-        #     if m.distance < 1*n.distance:
-        #         good_matches.append(m)
-        return good_matches, kp1, kp2
+        filtered_matches = [match for match, nearest_neighbor in matches if match.distance < 1 * nearest_neighbor.distance]
+        return filtered_matches, query_keypoints, train_keypoints
 
     def draw_matches(self):
-        # draw first 10 matches
-        img3 = cv2.drawMatchesKnn(self.img1, self.kp1, self.img2, self.kp2, self.good_matches, None, flags=2)
+        # Draw first 10 matches
+        img3 = cv2.drawMatchesKnn(self.query_image, self.query_keypoints, self.train_image, self.train_keypoints, self.filtered_matches, None, flags=2)
         plt.imshow(img3), plt.show()
     
     def get_uni_matches(self):
+    
         coord_array = []
-        for match in self.good_matches:
-            x1, y1 = self.kp1[match.queryIdx].pt
-            x2, y2 = self.kp2[match.trainIdx].pt
-            for x in [x1, y1, x2, y2]:
-                coord_array.append(x) 
-        # create list of UNIMatch objects
-        UNIlist = []
-        for i in range(0, len(coord_array), 4):
-            UNIlist.append(UNIMatch(coord_array[i], coord_array[i+1], coord_array[i+2], coord_array[i+3]))
+        for match in self.filtered_matches:
+            x1, y1 = self.query_keypoints[match.queryIdx].pt
+            x2, y2 = self.train_keypoints[match.trainIdx].pt
+            coord_array.extend([x1, y1, x2, y2])
 
-        return UNIlist
+        # Create list of UNIMatch objects
+        uniMatches = [UNIMatch(coord_array[i], coord_array[i + 1], coord_array[i + 2], coord_array[i + 3]) for i in range(0, len(coord_array), 4)]
+
+        return uniMatches
