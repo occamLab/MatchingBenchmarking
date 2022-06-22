7e4f2c2a67b26e42f2736eedccc4095ded029bc0
Anmol Sandhu <anmolrattansandhu@gmail.com>
2022-06-22 15:45:22 -0400

    added docstrings to MatchingAlgorithm and UniMatch

--- src/UniMatch.py
+++ src/UniMatch.py
@@ -1,4 +1,12 @@
 class UNIPoint:
+    """
+    A data structure representing a keypoint in an image.
+    Analogous to cv2.KeyPoint.
+
+    Instance Attributes:
+        x (float): The x coordinate of the keypoint.
+        y (float): The y coordinate of the keypoint.
+    """
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -8,6 +16,15 @@ class UNIPoint:
 
 
 class UNIMatch:
+    """
+    A data structure representing matched UNIPoints between two images.
+    This object was created to have one uniform match object since different
+    image matching algorithms and models use different data structures.
+
+    Instance Attributes:
+        queryPt (UNIPoint): The UNIPoint in the query image.
+        trainPt (UNIPoint): The UNIPoint in the train image.
+    """
     def __init__(self, x1, y1, x2, y2):
         self.queryPt = UNIPoint(x1, y1)
         self.trainPt = UNIPoint(x2, y2)
