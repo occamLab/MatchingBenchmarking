2f40667cd3e15ee6bb9d5fdb412e49fcf3511a88
Marc Eftimie <marc.eftimie@gmail.com>
2022-06-27 14:44:43 -0400

    Changed class architecture of MatchingAlgorithm"

--- src/MatchingAlgorithm.py
+++ src/MatchingAlgorithm.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 import cv2
 import matplotlib.pyplot as plt
 from UniMatch import UNIMatch
+
 # from SuperGlue import get_superglue_matches, draw_superglue_matches
 
 
@@ -13,6 +14,7 @@ class MatchingAlgorithm(ABC):
         query_image (numpy.ndarray): The query image passed into the algorithm.
         train_image (numpy.ndarray): The train image passed into the algorithm.
     """
+
     def __init__(self, query_image, train_image):
         # TODO remove images from constructor parameters, don't need to create a new object for each pair.
         self.query_image = query_image
@@ -41,7 +43,7 @@ class MatchingAlgorithm(ABC):
         pass
 
     @abstractmethod
-    def get_uni_matches(self):
+    def matches_to_unimatches(self):
         """
         Creates a list of UNIMatch objects representing the matches between the query and train images.
 
@@ -88,31 +90,29 @@ class OrbMatcher(MatchingAlgorithm):
     """
     Matching Algorithm that uses ORB to match keypoints between two images.
     """
-    def __init__(self, query_image, train_image):
-        super().__init__(query_image, train_image)
-        self.matches, self.query_keypoints, self.train_keypoints = self.get_matches()
 
-    def get_matches(self):
+    def __init__(self):
+        super().__init__()
+
+    def get_matches(self, query_image, train_image):
         # Initiate ORB algorithm
         orb = cv2.ORB_create()
 
         # Find the keypoints and descriptors with ORB
-        query_keypoints, query_descriptors = orb.detectAndCompute(
-            self.query_image, None
-        )
-        train_keypoints, train_descriptors = orb.detectAndCompute(
-            self.train_image, None
-        )
+        query_keypoints, query_descriptors = orb.detectAndCompute(query_image, None)
+        train_keypoints, train_descriptors = orb.detectAndCompute(train_image, None)
 
         # Get matches from keypoints and descriptors
         bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
         matches = bf.match(query_descriptors, train_descriptors)
 
         # TODO: Add ratio test for ORB algorithm
-        return (
-            sorted(matches, key=lambda x: x.distance),
-            query_keypoints,
-            train_keypoints,
+        return self.matches_to_unimatches(
+            (
+                sorted(matches, key=lambda x: x.distance),
+                query_keypoints,
+                train_keypoints,
+            )
         )
 
     def draw_matches(self):
@@ -129,12 +129,12 @@ class OrbMatcher(MatchingAlgorithm):
         plt.imshow(img3)
         plt.show(img3)
 
-    def get_uni_matches(self):
+    def matches_to_unimatches(self, matches, query_keypoints, train_keypoints):
         # Creates
         coord_array = []
-        for match in self.matches:
-            x1, y1 = self.query_keypoints[match.queryIdx].pt
-            x2, y2 = self.train_keypoints[match.trainIdx].pt
+        for match in matches:
+            x1, y1 = query_keypoints[match.queryIdx].pt
+            x2, y2 = train_keypoints[match.trainIdx].pt
             coord_array.extend([x1, y1, x2, y2])
 
         # Create list of UNIMatch objects
@@ -155,6 +155,7 @@ class SiftMatcher(MatchingAlgorithm):
     """
     Matching Algorithm that uses SIFT to match keypoints between two images.
     """
+
     def __init__(self, query_image, train_image):
         super().__init__(query_image, train_image)
         (
@@ -163,17 +164,13 @@ class SiftMatcher(MatchingAlgorithm):
             self.train_keypoints,
         ) = self.get_matches()
 
-    def get_matches(self):
+    def get_matches(self, query_image, train_image):
         # Initiate SIFT algorithm
         sift = cv2.SIFT_create()
 
         # Find the keypoints and descriptors with SIFT
-        query_keypoints, query_descriptors = sift.detectAndCompute(
-            self.query_image, None
-        )
-        train_keypoints, train_descriptors = sift.detectAndCompute(
-            self.train_image, None
-        )
+        query_keypoints, query_descriptors = sift.detectAndCompute(query_image, None)
+        train_keypoints, train_descriptors = sift.detectAndCompute(train_image, None)
 
         # Get matches from keypoints and descriptors
         bf = cv2.BFMatcher(cv2.NORM_L2)
@@ -185,7 +182,9 @@ class SiftMatcher(MatchingAlgorithm):
             for match, nearest_neighbor in matches
             if match.distance < 1 * nearest_neighbor.distance
         ]
-        return filtered_matches, query_keypoints, train_keypoints
+        return self.matches_to_unimatches(
+            filtered_matches, query_keypoints, train_keypoints
+        )
 
     def draw_matches(self):
         # Draw first 10 matches
@@ -200,12 +199,12 @@ class SiftMatcher(MatchingAlgorithm):
         )
         plt.imshow(img3), plt.show()
 
-    def get_uni_matches(self):
+    def matches_to_unimatches(self, matches, query_keypoints, train_keypoints):
 
         coord_array = []
-        for match in self.filtered_matches:
-            x1, y1 = self.query_keypoints[match.queryIdx].pt
-            x2, y2 = self.train_keypoints[match.trainIdx].pt
+        for match in matches:
+            x1, y1 = query_keypoints[match.queryIdx].pt
+            x2, y2 = train_keypoints[match.trainIdx].pt
             coord_array.extend([x1, y1, x2, y2])
 
         # Create list of UNIMatch objects
