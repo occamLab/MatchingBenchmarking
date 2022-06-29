b83c546ed7d304b195b06728c52fe90cf9bee8a3
Marc Eftimie <marc.eftimie@gmail.com>
2022-06-29 16:14:10 -0400

    Cleaned up DepthMatch

--- VisualizationTesting/DepthMath.py
+++ VisualizationTesting/DepthMath.py
@@ -12,7 +12,7 @@ from MatchingAlgorithm import OrbMatcher, SiftMatcher
 
 
 session = Benchmarker("Temp").sessions[0]
-bundle = session.bundles[0]
+bundle = session.bundles[11]
 new_sift_matcher = SiftMatcher().get_matches(bundle.query_image, bundle.train_image)
 
 
@@ -49,12 +49,8 @@ def map_depth(bundle):
         final_query_image = cv2.circle(
             query_image,
             (int(pixel[0]), int(pixel[1])),
-            0,
+            1,
             (
-                # TODO: Make a better system
-                # bundle.query_image_depth_map[i][3] * 255,
-                # bundle.query_image_depth_map[i][3] * 255,
-                # bundle.query_image_depth_map[i][3] * 255,
                 255,
                 255,
                 255,
@@ -62,12 +58,12 @@ def map_depth(bundle):
             -1,
         )
 
-    test_pixel = (1000, 1000)
-
+    # Represents the keypoint
+    test_pixel = (900, 500)
     final_query_image = cv2.circle(
         query_image,
         (test_pixel[0], test_pixel[1]),
-        10,
+        5,
         (
             255,
             255,
@@ -76,17 +72,19 @@ def map_depth(bundle):
         -1,
     )
 
+    # Converts pixels of keypoint to depth point index
     corresponding_depth_index = round(test_pixel[0] / 7.5) * 192 + round(
         test_pixel[1] / 7.5
     )
 
+    # Plots corresponding depth point
     final_query_image = cv2.circle(
         query_image,
         (
             int(pixels[corresponding_depth_index][0]),
             int(pixels[corresponding_depth_index][1]),
         ),
-        10,
+        5,
         (
             0,
             0,
@@ -95,6 +93,10 @@ def map_depth(bundle):
         -1,
     )
     cv2.imwrite(f"query.png", final_query_image)
+
+    focal_length = bundle.train_image_intrinsics[0]
+    offset_x = bundle.train_image_intrinsics[6]
+    offset_y = bundle.train_image_intrinsics[7]
 
     query_pose = np.array(bundle.query_image_pose).reshape(4, 4).T
     train_pose = np.array(bundle.train_image_pose).reshape(4, 4).T
@@ -122,25 +124,23 @@ def map_depth(bundle):
         final_train_image = cv2.circle(
             train_image,
             (int(pixel[0]), int(pixel[1])),
-            2,
+            1,
             (
                 bundle.query_image_depth_map[i][3] * 255,
                 bundle.query_image_depth_map[i][3] * 255,
                 bundle.query_image_depth_map[i][3] * 255,
-                # 255,
-                # 255,
-                # 255,
             ),
             -1,
         )
 
+    # Plots corresponding depth point from query image on trian image
     final_train_image = cv2.circle(
         train_image,
         (
             int(pixels[corresponding_depth_index][0]),
             int(pixels[corresponding_depth_index][1]),
         ),
-        10,
+        5,
         (
             0,
             0,
@@ -152,52 +152,3 @@ def map_depth(bundle):
 
 
 map_depth(bundle)
-
-# focal_length = intrinsics_dict["query"][0]
-# offset_x = intrinsics_dict["query"][6]
-# offset_y = intrinsics_dict["query"][7]
-
-# lidar_depth = []
-# for row in depth_dict["query"]:
-#     x = row[0] * row[3]
-#     y = row[1] * row[3]
-#     z = row[2] * row[3]
-#     lidar_depth.append([x, y, z])
-
-# query_pose = np.array(bundle.query_image_pose).reshape(4, 4).T
-# train_pose = np.array(bundle.train_image_pose).reshape(4, 4).T
-
-# print(query_pose)
-# print(train_pose)
-
-# relative_pose = inv(query_pose) @ train_pose
-
-# depth_data = np.array(lidar_depth)
-# depth_data = np.hstack((depth_data, np.ones((depth_data.shape[0], 1)))).T
-
-# depth_data_projected = inv(relative_pose) @ depth_data
-# depth_fp = np.array(
-#     (depth_data_projected[0], -depth_data_projected[1], -depth_data_projected[2])
-# ).T
-
-# pixels = []
-# for row in depth_fp:
-#     pixel_x = row[0] * focal_length / row[2] + offset_x
-#     pixel_y = row[1] * focal_length / row[2] + offset_y
-#     pixels.append((pixel_x, pixel_y))
-
-# final_image = bundle.train_image
-
-# for i, pixel in enumerate(pixels):
-#     img = cv2.circle(
-#         final_image,
-#         (int(pixel[0]), int(pixel[1])),
-#         10,
-#         (
-#             depth_dict["query"][i][3] * 256,
-#             depth_dict["query"][i][3] * 256,
-#             depth_dict["query"][i][3] * 256,
-#         ),
-#         -1,
-#     )
-# cv2.imwrite(f"train1.png", img)
